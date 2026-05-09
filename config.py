67e73fb899a192d9c0e3e45cec07e2fe0691fc8a
Laura Camila <lauracamilaav1@gmail.com>
2026-05-09 14:45:18 +0800

    Completed inputting IP addressing scheme

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import ipaddress
 
-network_topology = {\
-    
+network_topology = {
+
     #For use in the Network Layer
     "IP_addressing_scheme":{
-        "Network_1": ipaddress.ip_network('10.0.1.0/24'), #need to check if this format actually works
-        "Network_1": ipaddress.ip_network('10.0.2.0/24 ') #need to check if this format actually works
+        "Network_1": ipaddress.ip_network('10.0.1.0/24'), #need to check if this format actually works and will return the raw value
+        "Network_2": ipaddress.ip_network('10.0.2.0/24'), 
+        "Host_A": ipaddress.IPv4Address('10.0.1.10'),
+        "R1_I1": ipaddress.IPv4Address('10.0.1.1'),
+        "Host_B": ipaddress.IPv4Address('10.0.1.20'),
+        "R1_I2": ipaddress.IPv4Address('10.0.2.1')
     },
 
     #For use in the Data Link Layer
@@ -17,3 +21,4 @@ network_topology = {\
     }
 }
 
+print(network_topology["MAC_addressing_scheme"]["R1_I1"])
